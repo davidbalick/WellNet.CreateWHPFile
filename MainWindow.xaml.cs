d3b9e2beb05c43b571ae1d44c877ab86f0ad7e10
david balick <davidbalick@gmail.com>
2018-08-09 21:38:24 -0400

    Ver 1 complete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,10 +4,39 @@ namespace WellNet.CreateWHPFile
 {
     public partial class MainWindow : Window
     {
+        private ViewModel _viewModel;
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ViewModel();
+            _viewModel = new ViewModel();
+            DataContext = _viewModel;
+            TbExcelFilename.Drop += TbExcelFilename_Drop;
+            TbTextFilename.Drop += TbTextFilename_Drop;
+            TbScriptFilename.Drop += TbScriptFilename_Drop;
+        }
+
+        private void TbTextFilename_Drop(object sender, DragEventArgs e)
+        {
+            _viewModel.TextFilename = InterpretDragDropData(e.Data);
+        }
+        private void TbExcelFilename_Drop(object sender, DragEventArgs e)
+        {
+            _viewModel.ExcelFilename = InterpretDragDropData(e.Data);
+        }
+        private void TbScriptFilename_Drop(object sender, DragEventArgs e)
+        {
+            _viewModel.ScriptFilename = InterpretDragDropData(e.Data);
+        }
+
+        private static string InterpretDragDropData(IDataObject data)
+        {
+            var dataFormat = DataFormats.Text;
+            if (data.GetDataPresent(dataFormat))
+                return data.GetData(DataFormats.Text, true) as string;
+            dataFormat = DataFormats.FileDrop;
+            if (data.GetDataPresent(dataFormat))
+                return (data.GetData(dataFormat, true) as string[])[0];
+            return null;
         }
     }
 }
--- ViewModel.cs
+++ ViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WellNet.Excel;
 using WellNet.Sql;
 using WellNet.Utils;
 
@@ -17,9 +17,41 @@ namespace WellNet.CreateWHPFile
         private const string UI_CONN_NAME = "HSNavanex_WorkingDB";
         private const string GROUPLIST_SPROC = "WellnetUi.selectGroupList";
 
-        private Dictionary<string, string> _connMgr;
+        private BackgroundWorker _bgWorker;
 
         #region Properties
+        private Dictionary<string, string> _connMgr;
+        public Dictionary<string, string> Connections
+        {
+            get { return _connMgr; }
+            set
+            {
+                _connMgr = value;
+                OnPropertyChanged("Connections");
+            }
+        }
+        private string _conn;
+        public string Connection
+        {
+            get { return _conn; }
+            set
+            {
+                _conn = value;
+                OnPropertyChanged("Connection");
+            }
+        }
+        private string _table;
+        public string Table
+        {
+            get { return _table; }
+            set
+            {
+                _table = value;
+                OnPropertyChanged("Table");
+            }
+        }
+
+        public RelayCommand CreateFilesCommand { get; set; }
         private List<Group> _groups = null;
         public List<Group> Groups
         {
@@ -28,6 +60,16 @@ namespace WellNet.CreateWHPFile
             {
                 _groups = value;
                 OnPropertyChanged("Groups");
+            }
+        }
+        private Group _selectedGroup;
+        public Group SelectedGroup
+        {
+            get { return _selectedGroup; }
+            set
+            {
+                _selectedGroup = value;
+                OnPropertyChanged("SelectedGroup");
             }
         }
         private string _excelFilename = null;
@@ -50,11 +92,125 @@ namespace WellNet.CreateWHPFile
                 OnPropertyChanged("TextFilename");
             }
         }
+        private string _scriptFilename = null;
+        public string ScriptFilename
+        {
+            get { return _scriptFilename; }
+            set
+            {
+                _scriptFilename = Path.ChangeExtension(value, "sql");
+                OnPropertyChanged("ScriptFilename");
+            }
+        }
+        private DateTime? _dateOfInterest;
+        public DateTime? DateOfInterest
+        {
+            get { return _dateOfInterest; }
+            set
+            {
+                _dateOfInterest = value;
+                OnPropertyChanged("DateOfInterest");
+            }
+        }
+        private DateTime? _historyDate;
+        public DateTime? HistoryDate
+        {
+            get { return _historyDate; }
+            set
+            {
+                _historyDate = value;
+                OnPropertyChanged("HistoryDate");
+            }
+        }
+        private int? _effDateLookAhead;
+        public int? EffectiveDateLookAheadDays
+        {
+            get { return _effDateLookAhead; }
+            set
+            {
+                _effDateLookAhead = value;
+                OnPropertyChanged("EffectiveDateLookAheadDays");
+            }
+        }
+        private int? _termDateLookBack;
+        public int? TermDateLookBackDays
+        {
+            get { return _termDateLookBack; }
+            set
+            {
+                _termDateLookBack = value;
+                OnPropertyChanged("TermdateLookBackDays");
+            }
+        }
         #endregion Properties
 
         public ViewModel()
         {
             _connMgr = ConnectionManager.Create();
+            DateOfInterest = DateTime.Today;
+            HistoryDate = DateTime.Today.AddDays(1);
+            EffectiveDateLookAheadDays = 30;
+            TermDateLookBackDays = 0;
+            CreateFilesCommand = new RelayCommand(CreateFilesExec, CreateFilesCanExec);
+            _bgWorker = new BackgroundWorker { WorkerReportsProgress = true };
+            _bgWorker.DoWork += _bgWorker_DoWork;
+            _bgWorker.RunWorkerCompleted += _bgWorker_RunWorkerCompleted;
+            _bgWorker.ProgressChanged += _bgWorker_ProgressChanged;
+        }
+
+        private void _bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Status = e.UserState.ToString();
+        }
+
+        private void _bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                Status = e.Error.Message;
+            else
+                Status = "Completed";
+        }
+
+        private void _bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _bgWorker.ReportProgress(0, "Getting Data...");
+            var sqlHelper = SqlHelper.Create(_connMgr[WHP_CONN_NAME]);
+            var cmd = sqlHelper.CreateStoredProcCommand(WHP_SPROC, true);
+            cmd.Parameters["@ClientPrincipalID"].Value = SelectedGroup.Id;
+            cmd.Parameters["@DateOfInterest"].Value = DateOfInterest.Value;
+            cmd.Parameters["@HistoryDate"].Value = HistoryDate.Value;
+            cmd.Parameters["@EffDateLookAhead"].Value = EffectiveDateLookAheadDays.Value;
+            cmd.Parameters["@TermDateLookBackDays"].Value = TermDateLookBackDays.Value;
+            var dataTable = sqlHelper.PopulateDataTable(cmd);
+            _bgWorker.ReportProgress(0, "Creating Output...");
+            if (!string.IsNullOrEmpty(TextFilename))
+                DataToFile.TabDelimited(dataTable, TextFilename);
+            if (!string.IsNullOrEmpty(ExcelFilename))
+            {
+                var dataSet = new DataSet();
+                dataSet.Tables.Add(dataTable);
+                var dataToExcel = new DataToExcel();
+                dataToExcel.DataSetToExcel(dataSet, ExcelFilename, false);
+            }
+            if (!string.IsNullOrEmpty(ScriptFilename))
+                DataToFile.SqlScript(dataTable, ScriptFilename, Table);
+            if (!string.IsNullOrEmpty(Connection) && !string.IsNullOrEmpty(Table))
+            {
+                _bgWorker.ReportProgress(0, "Creating table...");
+                string sql;
+                if (!string.IsNullOrEmpty(ScriptFilename))
+                    sql = File.ReadAllText(ScriptFilename);
+                else
+                {
+                    var tempFile = Path.GetTempFileName();
+                    DataToFile.SqlScript(dataTable, tempFile, Table);
+                    sql = File.ReadAllText(tempFile);
+                    File.Delete(tempFile);
+                    sqlHelper = SqlHelper.Create(Connection);
+                    cmd = sqlHelper.CreateTextCommand(sql);
+                    sqlHelper.Execute(cmd);
+                }
+            }
         }
 
         private List<Group> GetGroups()
@@ -64,6 +220,22 @@ namespace WellNet.CreateWHPFile
             var dataTable = sqlHelper.PopulateDataTable(cmd);
             return dataTable.Rows.Cast<DataRow>().Select(dr =>
                 new Group { Id = new Guid(dr["ClientPrincipalID"].ToString()), Name = dr["GroupName"].ToString() }).ToList();
+        }
+
+        private bool CreateFilesCanExec(object arg)
+        {
+            return SelectedGroup != null && DateOfInterest.HasValue && HistoryDate.HasValue && EffectiveDateLookAheadDays.HasValue
+                && TermDateLookBackDays.HasValue && !_bgWorker.IsBusy &&
+                (
+                    (!string.IsNullOrEmpty(ExcelFilename) || !string.IsNullOrEmpty(TextFilename) || !string.IsNullOrEmpty(ScriptFilename))
+                    ||
+                    (!string.IsNullOrEmpty(Connection) && !string.IsNullOrEmpty(Table))
+                );
+        }
+
+        private void CreateFilesExec(object obj)
+        {
+            _bgWorker.RunWorkerAsync();
         }
     }
     public class Group
